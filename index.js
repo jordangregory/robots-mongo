c1821ad9d3a05eba202bd46085c1ba91af593a6e
Jordan Gregory <jordangregory@gmail.com>
2017-07-05 13:14:12 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const data = require("./data.js");
+const port = 4000;
+const mustacheExpress = require("mustache-express");
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./views");
+
+app.get("/", function(req, res) {
+  res.render("todo", data);
+});
+
+app.listen(port, function() {
+  console.log("Starting app on PORT: " + port + "...");
+});
